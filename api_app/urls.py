7eea6cefdf948dee44530abe22edfd88f3b960a0
VicTech Electronics <victechelectronics@gmail.com>
2023-05-24 15:28:01 +0300

    Delete

--- api_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.documentation),
-    path('call/', views.call),
-    path('report/', views.report),
-    path('message/', views.message),
-]
