ad16b61613b816732c0b724a19cc6f6fc02937a8
VicTech Electronics <victechelectronics@gmail.com>
2023-07-12 11:41:03 +0300

    Done

--- api_app/views.py
+++ api_app/views.py
@@ -2,6 +2,24 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import InfoSerializer
+from twilio.rest import Client
+
+
+# Twillio sms credentials
+account_sid = 'AC272eb7b173b88e71f4df1a34e788c52f'
+auth_token = '683809024cc03125653bad91cd2b4355'
+client = Client(account_sid, auth_token)
+twilio_phone = '+15734982063'
+client_phone = '+255719482668'
+
+# Metho to send sms
+def sendSMS(sms):
+    message = client.messages.create(
+        body=sms,
+        from_=twilio_phone,
+        to=client_phone
+    )
+    return message.sid
 
 # Create your views here.
 @api_view(['GET'])
@@ -27,6 +45,15 @@ def documentation(request):
 def informations(request):
     vehicle_number = request.data.get('vehicle_number')
     serializer = InfoSerializer(data = request.data)
+
+    latitude = request.data.get('latitude')
+    longitude = request.data.get('longitude')
+
+    sms_to_send = f'INFORMATION: \nAccident detected at. \nLatitude: {latitude} \nLongitude: {longitude} \nLink: https://tute-fyp22-23.herokuapp.com'
+    sms_response = sendSMS(sms_to_send)
+    print(f"Message sent! SID: {sms_response}")
+
+
     if serializer.is_valid():
         serializer.save()
         return Response('SUCCESS')
